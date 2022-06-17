cc390e8a3c17f55cfc7d974761401f33832122ad
Luciana Estevam <luestevam@hotmail.com>
2022-06-17 16:20:16 -0300

    🚧 Screen sing in - in progress

--- lib/src/auth/sing_in_screen.dart
+++ lib/src/auth/sing_in_screen.dart
@@ -1,5 +1,7 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengocer/src/auth/components/custom_text_field.dart';
+import 'package:greengocer/src/config/custom_colors.dart';
 
 class SingInScreen extends StatelessWidget {
   const SingInScreen({super.key});
@@ -7,19 +9,65 @@ class SingInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: CustomColors.customSwatchColor,
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           //PRIMEIRO CONTAINER
           Expanded(
-            child: Container(
-              color: Colors.red,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Nome do APP
+                Text.rich(
+                  TextSpan(
+                    style: const TextStyle(
+                      fontSize: 40,
+                    ),
+                    children: [
+                      const TextSpan(
+                        text: 'Green',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'grocer',
+                        style: TextStyle(
+                          color: CustomColors.customContrastColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                //CATEGORIAS
+                SizedBox(
+                  height: 30,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                      fontSize: 25,
+                    ),
+                    child: AnimatedTextKit(
+                      repeatForever: true,
+                      pause: Duration.zero,
+                      animatedTexts: [
+                        FadeAnimatedText('Frutas'),
+                        FadeAnimatedText('Verduras'),
+                        FadeAnimatedText('Legumes'),
+                        FadeAnimatedText('Carnes'),
+                        FadeAnimatedText('Cereais'),
+                        FadeAnimatedText('Laticínios'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
 
-          //SEGUNDO CONTAINER
-
+          //FORMULÁRIO CONTAINER
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
             decoration: const BoxDecoration(
@@ -31,6 +79,7 @@ class SingInScreen extends StatelessWidget {
 
             //Email
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 const CustomTextField(
                   icon: Icons.email,
@@ -66,16 +115,16 @@ class SingInScreen extends StatelessWidget {
                   alignment: Alignment.centerRight,
                   child: TextButton(
                     onPressed: () {},
-                    child: const Text(
+                    child: Text(
                       'Esqueceu a senha?',
-                      style: TextStyle(color: Colors.red),
+                      style: TextStyle(color: CustomColors.customContrastColor),
                     ),
                   ),
                 ),
 
                 //Divisor
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  padding: const EdgeInsets.only(bottom: 10),
                   child: Row(
                     children: [
                       Expanded(
@@ -102,14 +151,23 @@ class SingInScreen extends StatelessWidget {
                 SizedBox(
                   height: 50,
                   child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        side: BorderSide(
-                          width: 2,
-                          color: Colors.green,
-                        ),
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
                       ),
-                      onPressed: () {},
-                      child: const Text('Criar Conta')),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Colors.green,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Criar Conta',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
                 )
               ],
             ),
